4551873371f0c52abe1adf2d43693189ef41ccd5
EdominguezM <edominguez.ing@gmail.com>
2020-08-30 22:46:48 -0400

    fix page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :retweets, dependent: :destroy
 
