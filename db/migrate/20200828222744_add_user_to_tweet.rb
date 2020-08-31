fcd1f77ef20f7e29abaf133131a94436777ad9f2
EdominguezM <edominguez.ing@gmail.com>
2020-08-30 22:49:47 -0400

    fix page

--- db/migrate/20200828222744_add_user_to_tweet.rb
+++ db/migrate/20200828222744_add_user_to_tweet.rb
@@ -1,5 +1,5 @@
 class AddUserToTweet < ActiveRecord::Migration[6.0]
   def change
-    add_reference :tweets, :users, null: false, foreign_key: true
+    add_reference :tweets, :users, :integer, null: false, foreign_key: true
   end
 end
